67f4228ddc9128437c0be36c0efe4d14d7888c07
FedeBayer <fbayerque@alumnos.exa.unicen.edu.ar>
2022-11-25 16:24:29 -0300

    Added users list from api

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
+import fetch from "isomorphic-fetch";
 import Head from "next/head";
 import Container from "../components/Container";
-const Index = () => {
+import Users from "../components/users";
+const Index = (props) => {
   return (
     <div>
       <Container>
         <Head>
           <title>Next.js Project - Home</title>
         </Head>
-        <h1>Index</h1>
+        <h1>Next</h1>
+        <Users users={props.users} />
       </Container>
     </div>
   );
+};
+
+Index.getInitialProps = async (ctx) => {
+  const res = await fetch("https://reqres.in/api/users");
+  const resJSON = await res.json();
+  return { users: resJSON.data };
 };
 
 export default Index;
